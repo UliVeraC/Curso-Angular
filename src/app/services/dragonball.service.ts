27aa9b35d5d8471aa4715f3af9c86333b4ad8aed
UlisesVeraC <veracuevasu@gmail.com>
2025-04-17 20:25:31 -0600

    Proyecto1 del curso completado

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,12 +1,22 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { character } from '../interfaces/character.interface';
+
+const loadFromLocalStorage = (): character[] => {
+    const characters = localStorage.getItem('characters');
+
+    return characters ? JSON.parse(characters) : [];
+
+    return []
+}
 
 @Injectable({ providedIn: 'root' })
 export class DragonBallService {
-    characters = signal<character[]>([
-        { id: 1, name: 'Goku', power: 9000 },
-        { id: 2, name: 'Vegeta', power: 8000 },
-    ]);
+    characters = signal<character[]>(loadFromLocalStorage());
+
+    saveToLocalStorage = effect(() => {
+        localStorage.setItem('characters', JSON.stringify(this.characters()));
+    });
+
 
     addCharacter(character: character) {
         this.characters.update((list) => [...list, character]);
